d5b4a3f912f26dad683a321a506c28886d5ad195
far-galaxy <far.galaxy.klg@gmail.com>
2022-05-19 17:00:44 +0400

    added msg

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import *
+from python.lk_scrap import Scrapper
+import os
 
 app = Flask(__name__)
+scrapper = Scrapper()
 
 @app.route('/')
 def hello_world():
@@ -17,5 +20,17 @@ def get_files(path):
             abort(404)
     except FileNotFoundError:
         abort(404)
+        
+@app.route('/msg', methods=['GET'])
+def get():
+    
+    path = os.path.abspath("python/login.txt")
+    with open(path, "r", encoding="utf-8") as f:
+        login, password = f.read().split()  
+        
+    if scrapper.login(login, password):
+        chats = scrapper.method("chats", {"isArchive":0})
+        return chats
+    
 
 app.run(host="localhost", port=5000)
